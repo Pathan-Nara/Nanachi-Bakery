5886254513990ab09fd7bb4eac0cb6be76d4d9c3
Nawfel Dahri <nawfel.dahri@coda-student.school>
2025-11-13 14:02:42 +0100

    fix interface shop

--- Shop.py
+++ Shop.py
@@ -11,14 +11,12 @@ class Shop:
         self.width = 250
         self.buildings_types = BuildingList().get_buildings()
         self.item_rects = []
-
     def draw(self, player):
         n = len(self.buildings_types)
         padding_x = 10
         padding_y = 8
         spacing = 10
         title_height = 30
-
         infos = [b.get_info() for b in self.buildings_types]
         sizes = [SMALL_FONT.size(txt) for txt in infos]
         max_text_w = max((w for w, h in sizes), default=0)
@@ -36,11 +34,9 @@ class Shop:
             outer_x = screen_w - outer_width
         if outer_x < 0:
             outer_x = 0
-
         outer_rect = pygame.Rect(outer_x, outer_y, outer_width, outer_height)
         pygame.draw.rect(SCREEN, BLACK, outer_rect, 2)
         inner_x = outer_x + padding_x
-
         title_text = SMALL_FONT.render("Shop", True, BLACK)
         title_pos_x = inner_x + (box_width - title_text.get_width()) // 2
         title_pos_y = outer_y + padding_x
@@ -50,17 +46,13 @@ class Shop:
         for i, Building in enumerate(self.buildings_types):
             h = item_heights[i]
             color = GREEN if player.can_afford(Building.cost) else GRAY
-
             rect = pygame.Rect(inner_x, y_offset, box_width, h)
             pygame.draw.rect(SCREEN, color, rect)
             pygame.draw.rect(SCREEN, BLACK, rect, 2)
-
             text_surface = SMALL_FONT.render(infos[i], True, BLACK)
             SCREEN.blit(text_surface, (inner_x + padding_x - padding_x +10, y_offset + padding_y))
-
             self.item_rects.append((rect, Building))
             y_offset += h + spacing
-
     def handle_click(self, pos, player):
         for rect, Building in self.item_rects:
             if rect.collidepoint(pos):
